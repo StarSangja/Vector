64e7658b93f4c3d5c851b39dd70a1e23af44d4a5
StarSangja <starsangja@gmail.com>
2022-05-30 17:52:08 +0900

    Vector ver. 0.02

--- Vector/Vector/main.cpp
+++ Vector/Vector/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main(void)
 {
-	int str1[2][5]
+	char str1[2][5]
 	{
 		{'A','B','C','D','E'},
 		{'F','G','H','I','J'}
 	};
+	 
 
 	for (int i = 0; i < 5; i++)
 		cout << (*str1)[i];
