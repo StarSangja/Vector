00bc5a5e39330815c2fb36823c691f2efd663bae
StarSangja <starsangja@gmail.com>
2022-06-02 11:16:59 +0900

    Vector ver 0.2

--- Vector/Vector/main.cpp
+++ Vector/Vector/main.cpp
@@ -1,4 +1,4 @@
-// ** Vector ver 0.6
+// ** Vector ver 0.2
 
 #include <iostream>
 
@@ -6,22 +6,29 @@ using namespace std;
 
 int main(void)
 {
-	/* ======
+	 
 	
-
+	/*
 	char str1[2][5]
 	{
 		{'A','B','C','D','E'},
 		{'F','G','H','I','J'}
 	};
 	 
+	char* str2[2] =
+	{
+		{(char*)"ABCD"},
+		{(char*)"EFGH"}
+	};
 
 	for (int i = 0; i < 5; ++i)
 		cout << (*str1)[i];
 
 	cout << endl;
 	
-	===== */
+	printf_s("%s\n", str2[0]);
+	
+	*/
 	
 
 	return 0;
